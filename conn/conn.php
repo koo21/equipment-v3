e03da8f6b674470fa8bec820ff5d08eeb9f7acec
koo21 <sawaddiju@hotmail.com>
2023-10-25 14:21:13 +0700

    connect

---
+++ conn/conn.php
@@ -0,0 +1,16 @@
+<?php
+$host = 'localhost';
+$user = 'root';
+$pass = 'root';
+$db = 'db_main_tool';
+
+$coon = new PDO('mysql:host=$host;dbname=$db', $user, $pass);
+$coon->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$coon->exec('SET NAMES UTF8');
+date_default_timezone_set('Asia/Bangkok');
+
+if (empty($coon)) {
+    echo "No";
+} else {
+    echo "Yes";
+}
---
+++ pages/index.php
@@ -0,0 +1,2 @@
+<?php
+include_once '../conn/conn.php';
