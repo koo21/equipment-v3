a04a9122cb9e491cbeb4f309ab0263cb66506b2e
wasitakie <wasitaya@gmail.com>
2023-10-26 12:05:33 +0700

    new

--- mainPages/checkLogin.php
+++ mainPages/checkLogin.php
@@ -5,16 +5,18 @@ include '../conn/conn.php';
 echo "MainPage";
 
 
+// try {
+//     $stmt = $coon->prepare("SELECT*FROM admin WHERE admin_username = ? AND admin_password =?");
+//     $stmt->execute([$_POST["user"], $_POST["pass"]]);
+//     $row->rowCount($stmt);
+//     $r = $stmt->fetch(PDO::FETCH_ASSOC);
 
-$stmt = $coon->prepare("SELECT*FROM admin WHERE admin_username = ? AND admin_password =?");
-$stmt->execute([$_POST["user"], $_POST["pass"]]);
-$row->rowCount($stmt);
-$row = $stmt->fetch(PDO::FETCH_ASSOC);
-
-if (empty($stmt)) {
-    echo "don't connect ka";
-} else {
-    $_SESSION["checkLogin"] = $row["admin_check_login"];
-
-    header("location:pages/index.php");
-}
+//     if ($row == 0) {
+//         echo "don't connect ka";
+//     } else {
+//         echo  $_SESSION["checkLogin"] = $r["admin_check_login"];
+//         //header("location:pages/index.php");
+//     }
+// } catch (Exception $e) {
+//     echo "no" . $e->getMessage();
+// }
