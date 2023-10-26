4dca63a8d3493363f79dcd88113274bd8a5d65d3
koo21 <sawaddiju@hotmail.com>
2023-10-26 15:23:07 +0700

    piya

---
+++ mainPages/addTool.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../css/styles.css">
+    <link rel="stylesheet" href="../css/banner.css">
+    <link rel="stylesheet" href="../boot5/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
+    <script src="../boot5/js/bootstrap.bundle.min.js"></script>
+</head>
+
+<body>
+    <?php include '../banner/banner1.php'; ?>
+    <div class="container" style="margin-top: 15rem;">
+        <div class="col-md-12">
+            <h3>รายการทั้งหมด</h3>
+            <p>pitljgbgv</p>
+        </div>
+    </div>
+
+</body>
+
+</html>
