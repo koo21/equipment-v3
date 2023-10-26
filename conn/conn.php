ed09f660eba6b46c0ea2ef74c45735f839217d0a
wasitakie <wasitaya@gmail.com>
2023-10-26 14:14:27 +0700

    wawa

--- conn/conn.php
+++ conn/conn.php
@@ -8,3 +8,9 @@ $coon = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
 $coon->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $coon->exec('SET NAMES UTF8');
 date_default_timezone_set('Asia/Bangkok');
+
+// if (empty($coon)) {
+//     echo "No";
+// } else {
+//     echo "yes";
+// }
--- mainPages/checkLogin.php
+++ mainPages/checkLogin.php
@@ -1,22 +1,40 @@
+<link href="https://cdn.jsdelivr.net/npm/sweetalert2@11.7.32/dist/sweetalert2.min.css" rel="stylesheet">
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.7.32/dist/sweetalert2.all.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
 <?php
+
 session_start();
 include '../conn/conn.php';
 
-echo "MainPage";
+try {
 
-
-// try {
-//     $stmt = $coon->prepare("SELECT*FROM admin WHERE admin_username = ? AND admin_password =?");
-//     $stmt->execute([$_POST["user"], $_POST["pass"]]);
-//     $row->rowCount($stmt);
-//     $r = $stmt->fetch(PDO::FETCH_ASSOC);
-
-//     if ($row == 0) {
-//         echo "don't connect ka";
-//     } else {
-//         echo  $_SESSION["checkLogin"] = $r["admin_check_login"];
-//         //header("location:pages/index.php");
-//     }
-// } catch (Exception $e) {
-//     echo "no" . $e->getMessage();
-// }
+    $stmt = $coon->prepare(" SELECT* FROM admin WHERE admin_username = ? AND admin_password = ? ");
+    $stmt->execute([$_POST["user"], $_POST["pass"]]);
+    $row = $stmt->rowCount();
+    if ($row == 1) {
+        $r = $stmt->fetch(PDO::FETCH_ASSOC);
+        $_SESSION["adminName"] = $r["admin_name"];
+        header("location:mainPage.php");
+    } else {
+        echo "
+        <script>
+            $(document).ready(function(){
+                Swal.fire({
+                        title: 'ระบบงานครุภัณฑ์',
+                        text: 'ท่านไม่สามารถเข้าสู่ระบบได้ กรุณาทำการล็อกอินใหม่อีกครั้ง!',
+                        icon: 'error',
+                        confirmButtonColor: '#3085d6',
+                        confirmButtonText: 'กลับ'
+                    }).then((result) => {
+                        if (result.isConfirmed) {
+                            window.location.href = '../pages/index.php';
+                        }
+                    })
+            }) 
+        </script>
+        ";
+    }
+} catch (Exception $e) {
+    echo "no" . $e->getMessage();
+}
+?>
