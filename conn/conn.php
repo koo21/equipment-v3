9852427a628faa944681fa8289bfed0dbfd45192
koo21 <sawaddiju@hotmail.com>
2023-10-26 08:49:37 +0700

    update261066

--- conn/conn.php
+++ conn/conn.php
@@ -4,13 +4,7 @@ $user = 'root';
 $pass = 'root';
 $db = 'db_main_tool';
 
-$coon = new PDO('mysql:host=$host;dbname=$db', $user, $pass);
+$coon = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
 $coon->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $coon->exec('SET NAMES UTF8');
 date_default_timezone_set('Asia/Bangkok');
-
-if (empty($coon)) {
-    echo "No";
-} else {
-    echo "Yes";
-}
--- pages/index.php
+++ pages/index.php
@@ -1,2 +1,19 @@
-<?php
-include_once '../conn/conn.php';
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/styles.css">
+    <link rel="stylesheet" href="../css/banner.css">
+    <link rel="stylesheet" href="../boot5/css/bootstrap.min.css">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <?php include '../banner/banner.php'; ?>
+
+</body>
+
+</html>
