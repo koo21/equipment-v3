e9417282028f65decbf33e88a5f39bf66999e2a6
wasitakie <wasitaya@gmail.com>
2023-10-26 14:56:18 +0700

    add

---
+++ banner/banner1.php
@@ -0,0 +1,22 @@
+<div class="container-fluid menuMain1">
+    <div class="logo">
+        <img src="../images/ipsr_logo.png" alt="สถาบันวิจัยประชากรและสังคม
+            มหาวิทยาลัยมหิดล" srcset="">
+    </div>
+    <div class="menuBox1">
+        <ul>
+            <li><a href="#" data-bs-toggle="modal" data-bs-target="#modalLogin"><i class="bi bi-box-arrow-in-right"></i>
+                    เข้าสู่ระบบงานครุภัณฑ์</a></li>
+        </ul>
+    </div>
+</div>
+
+<?php include '../pages/modalLogin.php'; ?>
+
+<div class="container-fluid bannerMain1">
+    <div class="bannerText1">
+        <h2>ระบบงานครุภัณฑ์ และระบบแจ้งซ่อม</h2>
+        <p>สถาบันวิจัยประชากรและสังคม
+            มหาวิทยาลัยมหิดล</p>
+    </div>
+</div>
