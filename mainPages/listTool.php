84de265b9879150c487487d58bdafdaed38769d6
koo21 <sawaddiju@hotmail.com>
2023-11-03 10:11:13 +0700

    update_col-md

--- mainPages/listTool.php
+++ mainPages/listTool.php
@@ -85,7 +85,7 @@ include '../conn/conn.php';
                         $iconStatus = 'n.png';
                     }
                 ?>
-                    <div class="col-md-3 mb-3 ">
+                    <div class="col-xs-12 col-sm-6 col-md-4 col-xl-3 mb-5">
                         <div class="card">
                             <div class="cardFixSize">
                                 <div class="imgFix">
